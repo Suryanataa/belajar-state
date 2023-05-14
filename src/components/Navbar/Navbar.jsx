79580f9de06df212ba6bf0a1361f1acd7509e9bf
Surya <surya.nata.aardhana@gmail.com>
2023-05-14 15:26:08 +0700

    "add resto name on navbar"

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,17 +1,33 @@
+import { useState, useEffect } from "react";
 const Navbar = () => {
+	const [navbar, setNavbar] = useState(false);
+	const addRestoName = () => {
+		console.log(window.scrollY);
+		if (window.scrollY >= 66) {
+			setNavbar(true);
+		} else {
+			setNavbar(false);
+		}
+	};
+	useEffect(() => {
+		addRestoName();
+		// adding the event when scroll change Logo
+		window.addEventListener("scroll", addRestoName);
+	});
 	return (
 		<nav className="sticky top-0 flex items-center justify-between h-16 px-6 bg-white">
-			<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" className="bi bi-arrow-left" viewBox="0 0 24 24">
+			<svg xmlns="http://www.w3.org/2000/svg" width="19" height="19" fill="currentColor" className="bi bi-arrow-left" viewBox="0 0 16 16">
 				<path fillRule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z" />
 			</svg>
+			<h1 className={`mx-4 overflow-hidden font-semibold overflow-ellipsis whitespace-nowrap  ${navbar ? "" : "text-transparent"}`}>Warung Esmon, Bojong Nangka</h1>
 			<div className="flex">
-				<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" className="bi bi-search" viewBox="0 0 24 24">
+				<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-search " viewBox="0 0 16 16">
 					<path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
 				</svg>
-				<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" className="mx-6 bi bi-heart" viewBox="0 0 24 24">
+				<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="mx-6 bi bi-heart" viewBox="0 0 16 16">
 					<path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z" />
 				</svg>
-				<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" className="bi bi-share-fill" viewBox="0 0 24 24">
+				<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-share-fill" viewBox="0 0 16 16">
 					<path d="M11 2.5a2.5 2.5 0 1 1 .603 1.628l-6.718 3.12a2.499 2.499 0 0 1 0 1.504l6.718 3.12a2.5 2.5 0 1 1-.488.876l-6.718-3.12a2.5 2.5 0 1 1 0-3.256l6.718-3.12A2.5 2.5 0 0 1 11 2.5z" />
 				</svg>
 			</div>
